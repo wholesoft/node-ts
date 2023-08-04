69afc30a402ee8839881a4bb8b220c8041d200ef
Erik <69864741+wholesoft@users.noreply.github.com>
2023-08-03 22:32:56 -0700

    updated jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
-import { pathsToModuleNameMapper } from "ts-jest"
-
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/*.test.ts"],
-}
